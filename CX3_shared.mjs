b814afa44d0f5cda2af50b5ced23eb9ef886859e
eouia <eouia0819@gmail.com>
2023-04-25 16:31:01 +0200

    20230424

--- CX3_shared.mjs
+++ CX3_shared.mjs
@@ -58,9 +58,7 @@ const scheduledRefresh = ({refreshTimer, refreshInterval, job}) => {
   }, refreshInterval)
 }
 
-const renderEvent = (event, {
-  useSymbol
-}) => {
+const renderEventDefault = (event) => {
   let e = document.createElement('div')
   e.classList.add('event')
   event.calendarName ? e.classList.add('calendar_' + encodeURI(event.calendarName)) : null
@@ -88,6 +86,10 @@ const renderEvent = (event, {
 
   //console.log('>', this.instanceId) // TODO : Make sharable.
   oppositeMagic(e, event)
+  return e
+} 
+
+let renderSymbol = (e, event, useSymbol) => {
   if (useSymbol && Array.isArray(event.symbol) && event.symbol.length > 0) {
     event.symbol.forEach((symbol) => {
       let exDom = document.createElement('span')
@@ -107,11 +109,64 @@ const renderEvent = (event, {
     exDom.classList.add('noSymbol', 'symbol')
     e.appendChild(exDom)
   }
+}
+
+
+const renderEvent = (event, {
+  useSymbol
+}) => {
+  let e = renderEventDefault(event)
+  renderSymbol(e, event, useSymbol)
 
   let t = document.createElement('span') 
   t.classList.add('title', 'eventTitle')
   t.innerHTML = event.title
   e.appendChild(t)
+  return e
+}
+
+const renderEventAgenda = (event, {useSymbol, eventTimeOptions, locale}, tm)=> {
+  let e = renderEventDefault(event)
+
+  let headline = document.createElement('div')
+  headline.classList.add('headline')
+  renderSymbol(headline, event, useSymbol)
+
+  let time = document.createElement('div')
+  time.classList.add('period')
+
+  let startTime = document.createElement('div')
+  let st = new Date(event.startDate)
+  startTime.classList.add('time', 'startTime', (st.getDate() === tm.getDate()) ? 'inDay' : 'notInDay')
+  startTime.innerHTML = new Intl.DateTimeFormat(locale, eventTimeOptions).formatToParts(st).reduce((prev, cur, curIndex, arr) => {
+    prev = prev + `<span class="eventTimeParts ${cur.type} seq_${curIndex}">${cur.value}</span>`
+    return prev
+  }, '')
+  headline.appendChild(startTime)
+
+  let endTime = document.createElement('div')
+  let et = new Date(event.endDate)
+  endTime.classList.add('time', 'endTime', (et.getDate() === tm.getDate()) ? 'inDay' : 'notInDay')
+  endTime.innerHTML = new Intl.DateTimeFormat(locale, eventTimeOptions).formatToParts(et).reduce((prev, cur, curIndex, arr) => {
+    prev = prev + `<span class="eventTimeParts ${cur.type} seq_${curIndex}">${cur.value}</span>`
+    return prev
+  }, '')
+  headline.appendChild(endTime)
+
+  let title = document.createElement('div')
+  title.classList.add('title')
+  title.innerHTML = event.title
+  headline.appendChild(title)
+  e.appendChild(headline)
+  let description = document.createElement('div')
+  description.classList.add('description')
+  description.innerHTML = event.description || ''
+  e.appendChild(description)
+  let location = document.createElement('div')
+  location.classList.add('location')
+  location.innerHTML = event.location || ''
+  e.appendChild(location)
+
   return e
 }
 
@@ -239,6 +294,102 @@ const displayLegend = (dom, events, options = {}) => {
   dom.appendChild(lDom)
 }
 
+const isToday = (d) => {
+  let tm = new Date()
+  let start = (new Date(tm.valueOf())).setHours(0, 0, 0, 0)
+  let end = (new Date(tm.valueOf())).setHours(23, 59, 59, 999)
+  return (d.getTime() >= start && d.getTime() <= end)
+}
+
+const isThisMonth = (d) => {
+  let tm = new Date()
+  let start = new Date(tm.getFullYear(), tm.getMonth(), 1)
+  let end = new Date(start.getFullYear(), start.getMonth() + 1, 0, 23, 59, 59, 999)
+  return (d.getTime() >= start && d.getTime() <= end)
+}
+
+const isThisYear = (d) => {
+  let tm = new Date()
+  let start = new Date(tm.getFullYear(), 1, 1)
+  let end = new Date(tm.getFullYear(), 11, 31, 23, 59, 59, 999)
+  return (d.getTime() >= start && d.getTime() <= end)
+}
+
+const getBeginOfWeek = (d, options) => {
+  return new Date(d.getFullYear(), d.getMonth(), d.getDate() - (d.getDay() - options.firstDayOfWeek + 7 ) % 7)
+}
+
+const getEndOfWeek = (d, options) => {
+  let b = getBeginOfWeek(d, options)
+  return new Date(b.getFullYear(), b.getMonth(), b.getDate() + 6, 23, 59, 59, 999)
+}
+
+const getWeekNo = (d, options) => {
+  let bow = getBeginOfWeek(d, options)
+  let fw = getBeginOfWeek(new Date(d.getFullYear(), 0, options.minimalDaysOfNewYear), options)
+  if (bow.getTime() < fw.getTime()) fw = getBeginOfWeek(new Date(d.getFullYear() - 1, options), 0, options.minimalDayosOfNewYear)
+  let count = 1;
+  let t = new Date(fw.valueOf())
+  while (bow.getTime() > t.getTime()) {
+    t.setDate(t.getDate() + 7)
+    count++;
+  }
+  return count
+}
+
+const isPassed = (ev) => {
+  return (ev.endDate < Date.now())
+}
+
+const isFuture = (ev) => {
+  return (ev.startDate > Date.now())
+}
+
+const isCurrent = (ev) => {
+  let tm = Date.now()
+  return (ev.endDate >= tm && ev.startDate <= tm)
+}
+
+const isMultiday = (ev) => {
+  let s = new Date(ev.startDate)
+  let e = new Date(ev.endDate)
+  return ((s.getDate() !== e.getDate())
+    || (s.getMonth() !== e.getMonth())
+    || (s.getFullYear() !== e.getFullYear()))
+}
+
+const getRelativeDate = (d, index) => {
+  return new Date(d.getFullYear(), d.getMonth(), d.getDate() + index)
+}
+
+const gapFromToday = (d) => {
+  const MS = 24 * 60 * 60 * 1000
+  const t = new Date()
+  return Math.floor((Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()) - Date.UTC(t.getFullYear(), t.getMonth(), t.getDate())) / MS)
+}
+
+const makeWeatherDOM = (parentDom, forecasted) => {
+  if (forecasted && forecasted?.weatherType) {
+    let weatherDom = document.createElement('div')
+    weatherDom.classList.add('cellWeather')
+    let icon = document.createElement('span')
+    icon.classList.add('wi', 'wi-' + forecasted.weatherType)
+    weatherDom.appendChild(icon)
+    let maxTemp = document.createElement('span')
+    maxTemp.classList.add('maxTemp', 'temperature')
+    maxTemp.innerHTML = Math.round(forecasted.maxTemperature)
+    weatherDom.appendChild(maxTemp)
+    let minTemp = document.createElement('span')
+    minTemp.classList.add('minTemp', 'temperature')
+    minTemp.innerHTML = Math.round(forecasted.minTemperature)
+    weatherDom.appendChild(minTemp)
+    parentDom.appendChild(weatherDom)
+  }
+  return parentDom
+}
+
+
+
 export {
   uid,
   loaded,
@@ -247,6 +398,20 @@ export {
   //scheduledRefresh,
   prepareEvents,
   renderEvent,
+  renderEventAgenda,
   prepareMagic,
   displayLegend,
+  isToday,
+  isThisMonth,
+  isThisYear,
+  getBeginOfWeek,
+  getEndOfWeek,
+  getWeekNo,
+  isPassed,
+  isFuture,
+  isCurrent,
+  isMultiday,
+  getRelativeDate,
+  gapFromToday,
+  makeWeatherDOM
 }
